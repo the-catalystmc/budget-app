92fdfac931663de5bb7a1cd80a3a05d3f6aeb3e2
Rhyine <rhyinestewart@gmail.com>
2021-11-22 11:40:22 -0500

    Add group and report tables to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_160504) do
+ActiveRecord::Schema.define(version: 2021_11_22_163117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2021_11_22_160504) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
+  create_table "reports", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_reports_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +47,5 @@ ActiveRecord::Schema.define(version: 2021_11_22_160504) do
   end
 
   add_foreign_key "groups", "users"
+  add_foreign_key "reports", "users", column: "author_id"
 end
