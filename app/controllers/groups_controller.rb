db0baf0149f5a6a9e67c60f6f6eb064044e87ec1
Rhyine <rhyinestewart@gmail.com>
2021-11-23 12:31:52 -0500

    Add form for adding category

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,10 @@
 class GroupsController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    def new
-        @user = User.find(params[:user_id])
-        @current_user = current_user
-        @post = Post.new
-        respond_to do |format|
-          format.html { render :new }
-        end
+  def new
+    @group = Group.new
+    respond_to do |format|
+      format.html { render :new }
     end
+  end
 end
