725e3f815113994ce133a751e18319430e329412
Rhyine <rhyinestewart@gmail.com>
2021-11-23 11:14:21 -0500

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user_1 = User.create!(
+    name: 'user',
+    email: 'user@user.com',
+    password: 'password',
+    password_confirmation: 'password'
+    )
+    
+    user_2 = User.create!(
+    name: 'user_2',
+    email: 'user2@user.com',
+    password: 'password',
+    password_confirmation: 'password'
+    )
+    
+    food = Group.create(name: 'Food', icon: '', user_id: user_1.id)
+    bills = Group.create(name: 'Bills', icon: '', user_id: user_1.id)
+    outing = Group.create(name: 'Outing', icon: '', user_id: user_1.id)
+    travel = Group.create(name: 'Travel', icon: '', user_id: user_1.id)
+    
+    burger = Report.create(name: 'Burger', amount: 5, user_id: user_1.id)
+    movie = Report.create(name: 'Movie', amount: 10, user_id: user_1.id)
+    internet = Report.create(name: 'Internet', amount: 50, user_id: user_1.id)
+    rent = Report.create(name: 'Rent', amount: 200, user_id: user_1.id)
+    jamaica = Report.create(name: 'I went to Jamaica', amount: 10_000, user_id: user_1.id)
+    
+    food.reports << burger
+    bills.reports << internet
+    bills.reports << rent
+    outing.reports << jamaica
+    outing.reports << movie
