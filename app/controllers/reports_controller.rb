7bb148b96a90d938de433764cf5386c786c5f732
Rhyine <rhyinestewart@gmail.com>
2021-11-23 15:24:42 -0500

    Update the reports controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,20 @@
 class ReportsController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
+  def new
+    @report = Report.new
+  end
+
+  def create
+    user = current_user
+    report = user.reports.new(report_params)
+
+    if report.save
+      flash[:notice] = 'Category added successfully'
+      redirect_to reports_path
+    else
+      flash[:error] = report.errors.messages
+      render :new
+    end
+  end
 end
