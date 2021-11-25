abbb243e378f749676a8160abf12cdd26d6edc75
Rhyine <rhyinestewart@gmail.com>
2021-11-25 18:26:07 -0500

    Add linters and test config

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -9,16 +9,19 @@ class ReportsController < ApplicationController
   def create
     report = current_user.reports.create(report_params)
     group = Group.find(params[:group_id])
+    if params[:groups].nil?
+      flash[:alert] = 'Please select categories before submitting a new report'
+      return redirect_to group_path(group)
+    end
     params[:groups].each do |g|
       report.groups << Group.find(g)
     end
-
     if report.save
       flash[:notice] = 'Category added successfully'
       redirect_to group_path(group)
     else
       flash[:error] = report.errors.messages
-      render :new
+      redirect_to new_report_path(group_id: group)
     end
   end
 
